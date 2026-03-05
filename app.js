ca08dd66f805ee13884c49b36bdb6808ae1d2ef9
harish <hk04052006@gmail.com>
2026-03-05 14:13:04 +0000

    idk man

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 // the array for the books
 let myLibrary = [];
 
-function Book() {
-    // the constructor for the books...
+// some default additions of objects into the array
+book1 = new Book(crypto.randomUUID(), "Tokyo Ghoul", "Sui Ishida", "")
+
+// the constructor for the books
+function Book(id, name, authorName, readingStatus) {
+    if(!new.target){
+        throw Error(`Must use the new operator to call the function`);
+    }
+    this.id = id;
+    this.name = name;
+    this.authorName = authorName;
+    this.readingStatus = readingStatus;
 }
 
+// method for changing the reading status 
+Book.prototype.changeReadStatus = function(readingStatus) {
+    this.readingStatus = readingStatus;    
+}
+
+// function for taking in params, creating a book obj to then storing it in the array...
+function addBookToLibrary(obj) {
+    myLibrary.push(obj);
+}
+
+
+// first lets read our current html to add objects to our array 
+// then clear it all in the html
+// then loop through our array to then render it all on the html 
+// then keep an eye on the event listeners of each book, and do action of addition and deletion of the book accordingly 
+// then we go back in our game loop to rendering all the things back again... but i dont want this to be a loop... i want to attach the required functions that will implement the change of re-rendering when any such actions are performed
+
+// add event listeners to all the current books in our book list
